2989068706c6e3f3bda8f996ead375104690bc6b
adrian.trevino <adrian.chapa@catuxy.com>
2022-05-06 17:38:02 -0500

    edited user schema

--- website-demo-project/packages/backend/src/controllers/create-user/Controller.ts
+++ website-demo-project/packages/backend/src/controllers/create-user/Controller.ts
@@ -3,8 +3,14 @@ import { User } from "../../models/User";
 export async function ControllerCreateUser(req, res) {
 
   const user = new User({
-    name: "Bill",
     username: "billygates12",
+    password: "123",
+    name: "Bill",
+    lastName:"Gates",
+    age:60,
+    email:"billy.gates@gmail.com",
+    profilePicture:"https://i.imgur.com/dM7Thhn.png",
+    birthDate:new Date(),
   });
 
   const result = await user.save();
--- website-demo-project/packages/backend/src/models/User.ts
+++ website-demo-project/packages/backend/src/models/User.ts
@@ -2,12 +2,24 @@ import { Schema, model, connect } from "mongoose";
 
 export interface userInput {
   username: string;
+  password: string;
   name: string;
+  lastName: string;
+  age: number;
+  email: string;
+  profilePicture: string;
+  birthDate: Date;
 }
 
 export const userSchema = new Schema<userInput>({
   username: { type: String, required: true },
   name: { type: String, required: true },
+  password: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  profilePicture: { type: String, required: true },
+  birthDate: { type: Date, required: true },
 });
 
 export const User = model<userInput>("User", userSchema);
